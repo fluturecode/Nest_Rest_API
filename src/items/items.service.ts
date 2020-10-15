756f4580b4b4673f3c0d8a0a911b461f28646532
Elizabeth Eidelson <fluturecode@gmail.com>
2020-10-15 13:28:06 -0400

    added interfaces

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private readonly items: Item[] = [
+    {
+      id: '12345678',
+      name: 'Item One',
+      qty: 100,
+      description: 'This is item one',
+    },
+    {
+      id: '12345690',
+      name: 'Item Two',
+      qty: 50,
+      description: 'This is item two',
+    },
+  ];
+
+  findAll(): Item[] {
+    return this.items;
+  }
+}
